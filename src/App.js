6edabaa9388b1ec8f7192801c70d3aaae97a43f1
vchandu111 <vchanduu111@gmail.com>
2022-12-02 22:00:35 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ const App = () => {
   // Function is called everytime increment button is clicked
   const handleClick1 = () => {
     // Counter state is incremented
-    setCounter(counter + 8)
+    setCounter(counter + 4)
   }
   
   // Function is called everytime decrement button is clicked
   const handleClick2 = () => {
     // Counter state is decremented
-    setCounter(counter - 8)
+    setCounter(counter - 4)
   }
   
   // .yaml
